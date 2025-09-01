f7e6db9cd1d9278532512ce2960fc72966c508e2
Prathit Singh <114277302+prathit1@users.noreply.github.com>
2025-09-01 19:58:59 +0000

    revisit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,10 +12,6 @@ console.log(accountId);
 Prefer not to use var
 because of issue in block and funcitonal scope
 */
-
-
-
-
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
 
